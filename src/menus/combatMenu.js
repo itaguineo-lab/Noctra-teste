a6140a3458be177f85cb360f3abe38a0324d175c
itaguineo-lab <itaguineo@gmail.com>
2026-03-30 08:47:51 -0300

    Atualizar o combatMenu.js

--- src/menus/combatMenu.js
+++ src/menus/combatMenu.js
@@ -1,72 +1,36 @@
 const { Markup } = require('telegraf');
 
 function combatMenu(fight = null) {
-    const playerLowHp =
-        fight &&
-        fight.player &&
-        fight.player.hp <= fight.player.maxHp * 0.3;
-
-    const enemyLowHp =
-        fight &&
-        fight.enemy &&
-        fight.enemy.hp <= fight.enemy.maxHp * 0.25;
-
-    const attackLabel = enemyLowHp
-        ? '⚔️ Finalizar'
-        : '⚔️ Atacar';
-
-    const itemLabel = playerLowHp
-        ? '🧪 Curar'
-        : '🧪 Consumíveis';
-
+    const playerLowHp = fight && fight.player && fight.player.hp <= fight.player.maxHp * 0.3;
+    const enemyLowHp = fight && fight.enemy && fight.enemy.hp <= fight.enemy.maxHp * 0.25;
+    
+    const attackLabel = enemyLowHp ? '⚔️ Finalizar' : '⚔️ Atacar';
+    const skillLabel = fight && fight.player ? `✨ ${getSkillName(fight.player.className)}` : '✨ Habilidade';
+    
     return Markup.inlineKeyboard([
+        [Markup.button.callback(attackLabel, 'combat_attack')],
+        [Markup.button.callback(skillLabel, 'combat_skill')],
         [
-            Markup.button.callback(
-                attackLabel,
-                'combat_attack'
-            )
-        ],
-        [
-            Markup.button.callback(
-                itemLabel,
-                'combat_items'
-            ),
-            Markup.button.callback(
-                '💀 Almas',
-                'combat_souls'
-            )
-        ],
-        [
-            Markup.button.callback(
-                '🏃 Fugir',
-                'combat_flee'
-            )
+            Markup.button.callback('💀 Alma', 'combat_soul'),
+            Markup.button.callback('🏃 Fugir', 'combat_flee')
         ]
     ]);
+}
+
+function getSkillName(className) {
+    const map = {
+        guerreiro: 'Golpe Fraturante',
+        arqueiro: 'Flecha Sombria',
+        mago: 'Toque Gélido'
+    };
+    return map[className] || 'Habilidade';
 }
 
 function postCombatMenu() {
     return Markup.inlineKeyboard([
-        [
-            Markup.button.callback(
-                '⚔️ Caçar Novamente',
-                'hunt'
-            )
-        ],
-        [
-            Markup.button.callback(
-                '🎒 Inventário',
-                'inventory'
-            ),
-            Markup.button.callback(
-                '🗺️ Menu',
-                'menu'
-            )
-        ]
+        [Markup.button.callback('⚔️ Caçar Novamente', 'hunt')],
+        [Markup.button.callback('🎒 Inventário', 'inventory'), Markup.button.callback('🗺️ Menu', 'menu')]
     ]);
 }
 
-module.exports = {
-    combatMenu,
-    postCombatMenu
-};
+module.exports = { combatMenu, postCombatMenu };
