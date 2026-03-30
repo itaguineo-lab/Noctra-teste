1293e85d90bf3e55c18bedfa0bc59f7ad4576886
itaguineo-lab <itaguineo@gmail.com>
2026-03-30 12:24:48 -0300

    Atualizar o combatMenu.js

--- src/menus/combatMenu.js
+++ src/menus/combatMenu.js
@@ -1,14 +1,10 @@
 const { Markup } = require('telegraf');
 
 function combatMenu(fight = null) {
-    const playerLowHp = fight && fight.player && fight.player.hp <= fight.player.maxHp * 0.3;
-    const enemyLowHp = fight && fight.enemy && fight.enemy.hp <= fight.enemy.maxHp * 0.25;
-    
-    const attackLabel = enemyLowHp ? '⚔️ Finalizar' : '⚔️ Atacar';
     const skillLabel = fight && fight.player ? `✨ ${getSkillName(fight.player.className)}` : '✨ Habilidade';
     
     return Markup.inlineKeyboard([
-        [Markup.button.callback(attackLabel, 'combat_attack')],
+        [Markup.button.callback('⚔️ Atacar', 'combat_attack')],
         [Markup.button.callback(skillLabel, 'combat_skill')],
         [
             Markup.button.callback('💀 Alma', 'combat_soul'),
