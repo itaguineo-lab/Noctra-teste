15d4bc3238667570784a9875c0f510e4616cd005
itaguineo-lab <itaguineo@gmail.com>
2026-03-31 11:38:12 -0300

    Atualizar o combatMenu.js

--- src/menus/combatMenu.js
+++ src/menus/combatMenu.js
@@ -1,29 +1,54 @@
 const { Markup } = require('telegraf');
 
-function getSkillName(className) {
-  const map = {
-    guerreiro: 'Golpe Fraturante',
-    arqueiro: 'Flecha Sombria',
-    mago: 'Toque Gélido'
-  };
-  return map[className] || 'Habilidade';
-}
-
-function combatMenu(fight = null) {
-  const skillLabel = fight?.player ? `✨ ${getSkillName(fight.player.className)}` : '✨ Habilidade';
-
+function combatMenu() {
   return Markup.inlineKeyboard([
-    [Markup.button.callback('⚔️ Atacar', 'combat_attack')],
-    [Markup.button.callback(skillLabel, 'combat_skill')],
-    [Markup.button.callback('💀 Alma', 'combat_soul'), Markup.button.callback('🏃 Fugir', 'combat_flee')]
+    [
+      Markup.button.callback(
+        '🗡️ Atacar',
+        'attack'
+      )
+    ],
+    [
+      Markup.button.callback(
+        '💀 Almas',
+        'soul'
+      ),
+      Markup.button.callback(
+        '🧪 Consumíveis',
+        'consumables'
+      )
+    ],
+    [
+      Markup.button.callback(
+        '🏃 Fugir',
+        'flee'
+      )
+    ]
   ]);
 }
 
 function postCombatMenu() {
   return Markup.inlineKeyboard([
-    [Markup.button.callback('⚔️ Caçar Novamente', 'hunt')],
-    [Markup.button.callback('🎒 Inventário', 'inventory'), Markup.button.callback('🗺️ Menu', 'menu')]
+    [
+      Markup.button.callback(
+        '⚔️ Caçar Novamente',
+        'hunt'
+      )
+    ],
+    [
+      Markup.button.callback(
+        '🎒 Inventário',
+        'inventory'
+      ),
+      Markup.button.callback(
+        '🗺️ Menu',
+        'menu'
+      )
+    ]
   ]);
 }
 
-module.exports = { combatMenu, postCombatMenu };
+module.exports = {
+  combatMenu,
+  postCombatMenu
+};
