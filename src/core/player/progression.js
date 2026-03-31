e4ceb7821b2def64563f17d08c8e47da3a75d8d5
itaguineo-lab <itaguineo@gmail.com>
2026-03-31 19:49:49 -0300

    Atualizar o progression.js

--- src/core/player/progression.js
+++ src/core/player/progression.js
@@ -1,36 +1,24 @@
-const { recalculateStats } = require('./playerService');
-
 function getXpToNextLevel(level) {
-    return Math.floor(100 * Math.pow(level, 1.5));
+    // Fórmula: 100 * level^1.2 (progressão suave)
+    return Math.floor(100 * Math.pow(level, 1.2));
 }
 
 function addXp(player, amount) {
     player.xp += amount;
-
     return checkLevelUp(player);
 }
 
 function checkLevelUp(player) {
     let leveledUp = false;
-
     while (player.xp >= getXpToNextLevel(player.level)) {
         player.xp -= getXpToNextLevel(player.level);
         player.level++;
         leveledUp = true;
     }
-
     if (leveledUp) {
         recalculateStats(player);
-
         player.hp = player.maxHp;
         player.energy = player.maxEnergy;
     }
-
     return leveledUp;
 }
-
-module.exports = {
-    getXpToNextLevel,
-    addXp,
-    checkLevelUp
-};
