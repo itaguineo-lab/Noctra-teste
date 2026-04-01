3cc370eb2da0ae1172fd51ca301d836a3d1dc483
itaguineo-lab <itaguineo@gmail.com>
2026-04-01 10:53:55 -0300

    Atualizar o combatMenu.js

--- src/menus/combatMenu.js
+++ src/menus/combatMenu.js
@@ -1,54 +1,19 @@
 const { Markup } = require('telegraf');
 
 function combatMenu() {
-  return Markup.inlineKeyboard([
-    [
-      Markup.button.callback(
-        '🗡️ Atacar',
-        'attack'
-      )
-    ],
-    [
-      Markup.button.callback(
-        '💀 Almas',
-        'soul'
-      ),
-      Markup.button.callback(
-        '🧪 Poções',
-        'consumables'
-      )
-    ],
-    [
-      Markup.button.callback(
-        '🏃 Fugir',
-        'flee'
-      )
-    ]
-  ]);
+    return Markup.inlineKeyboard([
+        [Markup.button.callback('⚔️ Atacar', 'combat_attack')],
+        [Markup.button.callback('💀 Almas', 'combat_soul')],
+        [Markup.button.callback('🧪 Poções', 'combat_consumables')],
+        [Markup.button.callback('🏃 Fugir', 'combat_flee')]
+    ]);
 }
 
 function postCombatMenu() {
-  return Markup.inlineKeyboard([
-    [
-      Markup.button.callback(
-        '⚔️ Caçar Novamente',
-        'hunt'
-      )
-    ],
-    [
-      Markup.button.callback(
-        '🎒 Inventário',
-        'inventory'
-      ),
-      Markup.button.callback(
-        '🏠 Menu',
-        'menu'
-      )
-    ]
-  ]);
+    return Markup.inlineKeyboard([
+        [Markup.button.callback('⚔️ Caçar Novamente', 'hunt')],
+        [Markup.button.callback('🎒 Inventário', 'inventory'), Markup.button.callback('🗺️ Menu', 'menu')]
+    ]);
 }
 
-module.exports = {
-  combatMenu,
-  postCombatMenu
-};
+module.exports = { combatMenu, postCombatMenu };
