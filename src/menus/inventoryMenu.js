1a3d0b27f2a208a5d5fa39a87e1606fcfdbe086a
itaguineo-lab <itaguineo@gmail.com>
2026-04-01 23:06:02 -0300

    Atualizar o inventoryMenu.js

--- src/menus/inventoryMenu.js
+++ src/menus/inventoryMenu.js
@@ -1,46 +1,25 @@
 const { Markup } = require('telegraf');
 
 function inventoryCategoryMenu() {
-  return Markup.inlineKeyboard([
-    [
-      Markup.button.callback(
-        '⚔️ Armas',
-        'inv_weapons'
-      ),
-      Markup.button.callback(
-        '🛡️ Armaduras',
-        'inv_armors'
-      )
-    ],
-    [
-      Markup.button.callback(
-        '💎 Jóias',
-        'inv_jewelry'
-      ),
-      Markup.button.callback(
-        '🧪 Consumíveis',
-        'inv_consumables'
-      )
-    ],
-    [
-      Markup.button.callback(
-        '🎨 Skins',
-        'inv_skins'
-      ),
-      Markup.button.callback(
-        '💀 Almas',
-        'inv_souls'
-      )
-    ],
-    [
-      Markup.button.callback(
-        '🏠 Menu',
-        'menu'
-      )
-    ]
-  ]);
+    return Markup.inlineKeyboard([
+        [
+            Markup.button.callback('⚔️ Armas', 'inv_weapons'),
+            Markup.button.callback('🛡️ Armaduras', 'inv_armors')
+        ],
+        [
+            Markup.button.callback('💎 Joias', 'inv_jewelry'),
+            Markup.button.callback('👢 Botas', 'inv_boots')
+        ],
+        [
+            Markup.button.callback('🧪 Consumíveis', 'inv_consumables'),
+            Markup.button.callback('💀 Almas', 'inv_souls')
+        ],
+        [
+            Markup.button.callback('🏠 Menu', 'menu')
+        ]
+    ]);
 }
 
 module.exports = {
-  inventoryCategoryMenu
+    inventoryCategoryMenu
 };
