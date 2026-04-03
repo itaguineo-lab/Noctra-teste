7866c1cf4c81e78429d0b022d5cdb944ec7c40b2
itaguineo-lab <itaguineo@gmail.com>
2026-04-03 06:48:02 -0300

    Atualizar o inventoryMenu.js

--- src/menus/inventoryMenu.js
+++ src/menus/inventoryMenu.js
@@ -7,12 +7,15 @@ function inventoryCategoryMenu() {
             Markup.button.callback('🛡️ Armaduras', 'inv_armors')
         ],
         [
-            Markup.button.callback('💎 Joias', 'inv_jewelry'),
+            Markup.button.callback('💍 Joias', 'inv_jewelry'),
             Markup.button.callback('👢 Botas', 'inv_boots')
         ],
         [
             Markup.button.callback('🧪 Consumíveis', 'inv_consumables'),
             Markup.button.callback('💀 Almas', 'inv_souls')
+        ],
+        [
+            Markup.button.callback('✨ Auto Equip', 'auto_equip')
         ],
         [
             Markup.button.callback('🏠 Menu', 'menu')
