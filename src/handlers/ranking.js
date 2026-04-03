d325d49e9efdbeb770dfda26ed57f20d0b031eff
itaguineo-lab <itaguineo@gmail.com>
2026-04-03 12:18:54 -0300

    Criar ranking.js

---
+++ src/handlers/ranking.js
@@ -0,0 +1,41 @@
+const { getAllPlayers } = require('../core/player/playerService');
+const { Markup } = require('telegraf');
+
+async function handleRanking(ctx) {
+    const players = getAllPlayers();
+    const list = Object.values(players)
+        .filter(p => p && p.id)
+        .map(p => ({
+            id: p.id,
+            name: p.name || 'Desconhecido',
+            level: p.level || 1,
+            kills: p.totalKills || 0
+        }));
+
+    // Top 10 por nível
+    const topLevel = [...list].sort((a,b) => b.level - a.level).slice(0,10);
+    // Top 10 por kills
+    const topKills = [...list].sort((a,b) => b.kills - a.kills).slice(0,10);
+
+    let msg = `🏆 *RANKING GLOBAL*\n\n`;
+    msg += `*📈 Por Nível:*\n`;
+    topLevel.forEach((p, idx) => {
+        msg += `${idx+1}. ${p.name} - Nv ${p.level}\n`;
+    });
+    msg += `\n*⚔️ Por Abates:*\n`;
+    topKills.forEach((p, idx) => {
+        msg += `${idx+1}. ${p.name} - ${p.kills} kills\n`;
+    });
+
+    const keyboard = Markup.inlineKeyboard([
+        [Markup.button.callback('◀️ Voltar', 'menu')]
+    ]);
+
+    if (ctx.callbackQuery) {
+        await ctx.editMessageText(msg, { parse_mode: 'Markdown', ...keyboard });
+    } else {
+        await ctx.reply(msg, { parse_mode: 'Markdown', ...keyboard });
+    }
+}
+
+module.exports = { handleRanking };
