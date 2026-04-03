c09aa5db99c53fadf9d8b237b3941cf14b4b289d
itaguineo-lab <itaguineo@gmail.com>
2026-04-03 06:41:00 -0300

    Atualizar o mainMenu.js

--- src/menus/mainMenu.js
+++ src/menus/mainMenu.js
@@ -1,13 +1,30 @@
 const { Markup } = require('telegraf');
 
 function mainMenu() {
-  return Markup.inlineKeyboard([
-    [Markup.button.callback('⚔️ Caçar', 'hunt'), Markup.button.callback('🗺️ Viajar', 'travel')],
-    [Markup.button.callback('🎒 Inventário', 'inventory'), Markup.button.callback('👤 Perfil', 'profile')],
-    [Markup.button.callback('🛒 Loja', 'shop'), Markup.button.callback('⚡ Energia', 'energy')],
-    [Markup.button.callback('💎 VIP', 'vip'), Markup.button.callback('👥 Online', 'online')],
-    [Markup.button.callback('🎁 Baú Diário', 'daily')]
-  ]);
+    return Markup.inlineKeyboard([
+        [
+            Markup.button.callback('⚔️ Caçar', 'hunt'),
+            Markup.button.callback('🏰 Masmorra', 'dungeon')
+        ],
+        [
+            Markup.button.callback('🗺️ Viajar', 'travel'),
+            Markup.button.callback('🎒 Inventário', 'inventory')
+        ],
+        [
+            Markup.button.callback('👤 Perfil', 'profile'),
+            Markup.button.callback('🛒 Loja', 'shop')
+        ],
+        [
+            Markup.button.callback('⚡ Energia', 'energy'),
+            Markup.button.callback('🎁 Diário', 'daily')
+        ],
+        [
+            Markup.button.callback('💎 VIP', 'vip'),
+            Markup.button.callback('👥 Online', 'online')
+        ]
+    ]);
 }
 
-module.exports = { mainMenu };
+module.exports = {
+    mainMenu
+};
