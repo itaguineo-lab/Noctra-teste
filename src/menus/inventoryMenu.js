770c68310e5c52bc46154bcf36532e1bd4607a35
itaguineo-lab <itaguineo@gmail.com>
2026-04-09 23:35:27 -0300

    Atualizar o inventoryMenu.js

--- src/menus/inventoryMenu.js
+++ src/menus/inventoryMenu.js
@@ -12,6 +12,7 @@ function countItemsBySlots(player = {}, slots = []) {
 
 function inventoryMainMenu(player = {}) {
     const weapons = countItemsBySlots(player, ['weapon']);
+    const shields = countItemsBySlots(player, ['shield']);
     const armors = countItemsBySlots(player, ['armor']);
     const necklaces = countItemsBySlots(player, ['necklace']);
     const rings = countItemsBySlots(player, ['ring']);
@@ -23,21 +24,22 @@ function inventoryMainMenu(player = {}) {
     return Markup.inlineKeyboard([
         [
             Markup.button.callback(`⚔️ Armas (${weapons})`, 'invcat:weapons'),
-            Markup.button.callback(`🛡️ Armaduras (${armors})`, 'invcat:armors')
+            Markup.button.callback(`🛡️ Escudos (${shields})`, 'invcat:shields')
+        ],
+        [
+            Markup.button.callback(`🥋 Armaduras (${armors})`, 'invcat:armors'),
+            Markup.button.callback(`👢 Botas (${boots})`, 'invcat:boots')
         ],
         [
             Markup.button.callback(`📿 Amuletos (${necklaces})`, 'invcat:necklaces'),
             Markup.button.callback(`💍 Anéis (${rings})`, 'invcat:rings')
         ],
         [
-            Markup.button.callback(`👢 Botas (${boots})`, 'invcat:boots'),
-            Markup.button.callback(`🧪 Consumíveis (${consumables})`, 'invcat:consumables')
-        ],
-        [
-            Markup.button.callback(`🎨 Skins (${skins})`, 'invcat:skins'),
+            Markup.button.callback(`🧪 Consumíveis (${consumables})`, 'invcat:consumables'),
             Markup.button.callback(`💀 Almas (${souls})`, 'invcat:souls')
         ],
         [
+            Markup.button.callback(`🎨 Skins (${skins})`, 'invcat:skins'),
             Markup.button.callback('🏠 Menu', 'menu')
         ]
     ]);
