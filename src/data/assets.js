e9fd0475a05bbcee8fbe258fde6a4942b1662457
itaguineo-lab <itaguineo@gmail.com>
2026-04-13 21:39:00 -0300

    Criar assets.js

---
+++ src/data/assets.js
@@ -0,0 +1,35 @@
+module.exports = {
+    enemies: {
+        shadow_wolf: '',
+        giant_rat: '',
+        forest_spider: '',
+        dark_bat: '',
+        alpha_shadow_wolf: '',
+        webspinner: '',
+        dark_stag: '',
+        forest_guardian: ''
+    },
+
+    maps: {
+        clareira_sombria: '',
+        cripta_em_ruinas: '',
+        pantano_corrompido: '',
+        deserto_incandescente: '',
+        citadela_lunar: '',
+        abismo_noctra: ''
+    },
+
+    profile: {
+        warrior_lvl1: '',
+        archer_lvl1: '',
+        mage_lvl1: ''
+    },
+
+    bosses: {
+        noctra_avatar: ''
+    },
+
+    items: {},
+
+    ui: {}
+};
