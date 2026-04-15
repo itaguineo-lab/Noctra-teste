4f9c81d8b9e3b1bf2ba181c504d98a14d25b186f
itaguineo-lab <itaguineo@gmail.com>
2026-04-15 18:48:48 -0300

    Criar playerMutations.js

---
+++ src/core/player/playerMutations.js
@@ -0,0 +1,21 @@
+applyDamage(player, amount)
+applyHeal(player, amount)
+restoreFullHp(player)
+consumeEnergy(player, amount)
+restoreEnergy(player, amount)
+
+applyEquipmentChange(player, slot, item)
+removeEquipment(player, slot)
+
+applySoulEquip(player, soul)
+applySoulUnequip(player, slot)
+
+applyGoldReward(player, amount)
+applyNoxReward(player, amount)
+applyKeyReward(player, amount)
+applyXpReward(player, amount)
+
+applyBuff(player, buff)
+cleanupExpiredBuffs(player)
+
+normalizePlayerForSave(player)
