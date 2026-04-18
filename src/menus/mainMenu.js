2b247d15c3717bc7dc8b99021bed196861c0f401
itaguineo-lab <itaguineo@gmail.com>
2026-04-18 07:03:00 -0300

    Atualizar o mainMenu.js

--- src/menus/mainMenu.js
+++ src/menus/mainMenu.js
@@ -4,22 +4,22 @@ function mainMenu() {
     return Markup.inlineKeyboard([
         [
             Markup.button.callback('⚔️ Caçar', 'hunt'),
-            Markup.button.callback('🏰 Masmorra', 'dungeon')
+            Markup.button.callback('🗺️ Viajar', 'travel')
         ],
         [
-            Markup.button.callback('🗺️ Viajar', 'travel'),
-            Markup.button.callback('🎒 Inventário', 'inventory')
+            Markup.button.callback('🏰 Masmorra', 'dungeon'),
+            Markup.button.callback('🏟️ Arena', 'arena')
         ],
         [
             Markup.button.callback('👤 Perfil', 'profile'),
-            Markup.button.callback('🛒 Loja', 'shop')
+            Markup.button.callback('🎒 Inventário', 'inventory')
         ],
         [
-            Markup.button.callback('🎁 Diário', 'daily'),
-            Markup.button.callback('⚡ Energia', 'energy')
+            Markup.button.callback('⚡ Energia', 'energy'),
+            Markup.button.callback('🎁 Diário', 'daily')
         ],
         [
-            Markup.button.callback('🏟️ Arena', 'arena'),
+            Markup.button.callback('🛒 Loja', 'shop'),
             Markup.button.callback('🏆 Ranking', 'ranking')
         ],
         [
